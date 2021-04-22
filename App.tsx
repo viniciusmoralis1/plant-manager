15397c52893c4bba8cdd72688abe460a326aa881
Vinicius Moralis <vinicius_moralis@hotmail.com>
2021-04-22 18:31:08 -0300

    iniciando rotas com stack

--- App.tsx
+++ App.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import AppLoading from 'expo-app-loading';
-import { Welcome } from './src/pages/Welcome';
-import { Identification } from './src/pages/Identification';
-import { Confirmation } from './src/pages/Confirmation';
+import Routes from './src/routes';
 import { useFonts, Jost_400Regular, Jost_600SemiBold } from '@expo-google-fonts/jost';
 
 export default function App() {
@@ -15,8 +13,6 @@ export default function App() {
     return <AppLoading />
 
   return (
-    <>
-      <Confirmation />
-    </>
+    <Routes />
   );
 }
